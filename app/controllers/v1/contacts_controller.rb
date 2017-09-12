0b5e8ebc20083928ce568b30e0edc5f2d5d4a581
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-09-12 17:32:28 -0400

    added create and delete method for contact controller

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -4,4 +4,27 @@ class V1::ContactsController < ApplicationController
 
     render json: @contacts, status: :ok
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+
+    render json: @contact, status: :created
+  end
+
+  def destroy
+    @contact = Contact.find_by(id: params[:id])
+    if @contact.destroy
+      head(:ok)
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email)
+  end
+
 end
