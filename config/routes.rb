54a76931afc29a2e6da4966ad18a870ba234754a
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-09-12 21:07:52 -0400

    added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  # devise_for :users
+
   namespace :v1 do
     resources :contacts
+    resources :sessions, only: [:create, :destroy]
   end
 
 end
